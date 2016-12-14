f3aa73319d71d7162ae55d4a58e5fca9065e40ee
Víctor D. Saldaña C <Vctor D. Saldaa C>
2016-12-14 17:35:08 +0100

    reorder2.1

---
+++ M2.PA1_Pollutant_mean.R
@@ -0,0 +1,11 @@
+pollutantmean<-function(directory,pollutant,id=1:332){
+        setwd("C:/Victor/Estudios/6.MOOCs/RProgramming/ProgAssig1")
+        directory<-"C:/Victor/Estudios/6.MOOCs/RProgramming/ProgAssig1"
+        files_full<-list.files(directory)
+        dat<-data.frame()
+        for (i in id){
+                dat<-rbind(dat, read.csv(files_full[i]))
+        } 
+        dat_subset<-dat[,pollutant]
+        mean(dat_subset,na.rm=TRUE)
+}
